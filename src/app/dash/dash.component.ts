bef389c7f0b47a419ee6ca59c427b48d6f1442ae
Rafael Carvalho <rapha.pse@outlook.com>
2020-01-18 02:41:22 -0300

    Finished cars list

--- src/app/dash/dash.component.ts
+++ src/app/dash/dash.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'ctm-dash',
   templateUrl: './dash.component.html',
   styleUrls: ['./dash.component.scss']
 })
-export class DashComponent implements OnInit {
+export class DashComponent {
 
-  constructor() { }
 
-  ngOnInit() {
-  }
+  public cards = [
+    {
+      uuid: '1232kjkj399-32882730-0h231--3230',
+      color: 1,
+      typeClient: 'lead',
+      name: 'Rafael',
+      firstName: 'Carvalho Caetano'
+    },
+    {
+      uuid: '1232kjkj399-32882730-30',
+      color: 2,
+      typeClient: 'client',
+      name: 'Rosemeire',
+      firstName: 'Ribeiro Braz'
+    },
+    {
+      uuid: '4rt616g-9---9-9-9-923812321',
+      color: 1,
+      typeClient: 'lead',
+      name: 'Vitor',
+      firstName: 'Ribeiro Caetano'
+    },
+    {
+      uuid: '4rt616g-9923812321',
+      color: 1,
+      typeClient: 'client',
+      name: 'Heitor',
+      firstName: 'Ribeiro Caetano'
+    }
+  ];
 
 }
--- src/app/shared/components/cards/cards.component.ts
+++ src/app/shared/components/cards/cards.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'ctm-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent implements OnInit {
+export class CardsComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  @Input() card = {
+    uuid: null,
+    color: 1,
+    typeClient: null,
+    name: null,
+    firstName: null
+  };
 
 }
