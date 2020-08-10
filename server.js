887ee5ca6a6f715dfe2532c928caee8562f44063
rafael <rapha.pse@outlook.com>
2020-08-10 20:33:43 -0300

    Adding server

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const server = express();
+
+server.use(express.static(__dirname + '/dist'));
+server.get('/', (req, resp) => resp.sendFile(path.join(__dirname + './dist/index.html')));
+
+server.listen(4200);
